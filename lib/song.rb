6c6f496eaef22ff5ee6ab73be7962b0326f1ea6c
Hope Gipson <gipson.hope@gmail.com>
2020-07-08 02:01:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,5 +33,17 @@ class Song
     genre_count
   end
   
+    def Song.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+  
   
 end
